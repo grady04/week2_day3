ac589f5a3b55da40107b0d832717ad40bd6c52e7
grady04 <101082568+grady04@users.noreply.github.com>
2022-04-14 18:34:01 -0500

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,8 @@
+def area_of_house():
+    width = int(input("What is the width of the house in ft? "))
+    length = int(input("What is the length of the house in ft? "))
+    print(f"{width * length} ft^2")
+
+def circle_circumference():
+    radius = int(input("What is the radius of the circle? "))
+    print(f"the circumference is {radius * 2 * 3.1415}")
